a0b86d1dabe2afba27d82aca18da6ba5291439a2
Karim <k.robai@yahoo.ca>
2025-10-31 01:54:35 -0400

    Add files via upload

---
+++ ItemManager/Services/CategoryService.cs
@@ -0,0 +1,44 @@
+﻿using ItemManagerClient.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ItemManagerClient.Services
+{
+   /// <summary>
+   /// Category service implementation
+   /// </summary>
+   public class CategoryService : ICategoryService
+   {
+      private readonly IDataService _dataService;
+
+      public CategoryService(IDataService dataService)
+      {
+         _dataService = dataService;
+      }
+
+      public async Task<List<Category>> GetCategoriesAsync()
+      {
+         return await _dataService.GetAllCategoriesAsync();
+      }
+
+      public async Task<Category> GetCategoryAsync(int id)
+      {
+         return await _dataService.GetCategoryByIdAsync(id);
+      }
+
+      public async Task CreateCategoryAsync(Category category)
+      {
+         await _dataService.AddCategoryAsync(category);
+      }
+
+      public async Task UpdateCategoryAsync(Category category)
+      {
+         await _dataService.UpdateCategoryAsync(category);
+      }
+
+      public async Task DeleteCategoryAsync(int id)
+      {
+         await _dataService.DeleteCategoryAsync(id);
+      }
+   }
+}
