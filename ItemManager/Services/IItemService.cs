9f9003f549b8974af9f02b1fb8c3bb356d2c140e
Karim <k.robai@yahoo.ca>
2025-10-31 23:15:34 -0400

    Add files via upload

---
+++ ItemManager/Services/IItemService.cs
@@ -0,0 +1,22 @@
+﻿using ItemManagerClient.Enums;
+using ItemManagerClient.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ItemManagerClient.Services
+{
+   /// <summary>
+   /// Item-specific service interface
+   /// </summary>
+   public interface IItemService
+   {
+      Task<List<Item>> GetItemsAsync();
+      Task<Item> GetItemAsync(int id);
+      Task CreateItemAsync(Item item);
+      Task UpdateItemAsync(Item item);
+      Task DeleteItemAsync(int id);
+      Task<List<Item>> GetItemsByStatusAsync(ItemStatus status);
+      Task<List<Item>> GetItemsByCategoryAsync(int categoryId);
+      Task<List<Item>> SearchItemsAsync(string searchTerm);
+   }
+}
